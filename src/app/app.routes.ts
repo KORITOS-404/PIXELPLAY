e237386b79c05c9c0ed8512a37de631fcbcb6331
KevinChavez025 <kevinchavezgeldres@gmail.com>
2025-10-01 16:01:57 -0500

    Añadiendo el apartado Nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Nosotros } from './pages/nosotros/nosotros';
 
   export const routes: Routes = [
     { path: '', component: Home },
     { path: 'home', component: Home },
     { path:'login',component:Login},
-    {path:'register',component:Register}
+    {path:'register',component:Register},
+    {path: 'nosotros',component:Nosotros}
   ];
